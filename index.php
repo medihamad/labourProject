1cc71a80062635723c7373ecb0202348c85cad0f
Medi hamad <kumarpatel692@gmail.com>
2018-11-23 11:21:22 +0300

    adding files

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/nav.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="welcome">
+        <header>
+           <img src="Images/banner_1.png"/>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php">About us</a>
+                        <ul>
+                            <li><a href="#"> WORKPLACE HEALTH AND SAFETY </a></li>
+                            <li><a href="#"> THE GENDER COMPONENT </a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php">Services</a>
+                        <ul><li><a href="#"> HEALTH THROUGH SPORT </a></li>
+                            <li><a href="#"> WORKPLACE HEALTH AND SAFETY </a></li>
+                            <li><a href="#"> THE GENDER COMPONENT </a></li>
+                            <li><a href="#"> ROAD SAFETY COMPONENT  </a></li>
+                            <li><a href="#"> THE LABOUR RELATIONSHIP </a></li>
+                            <li><a href="#"> ENVIRONMENTAL PROTECTION  </a></li>
+                            <li><a href="#"> THE MAIN COMPONENTS  </a></li>
+                            <li><a href="#"> PROVISION OF PPE   </a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php">Contact us</a></li>
+                    <li class="lastlink"><a href="index.php">Projects</a></li>
+                </ul>
+            </nav>
+        </header> 
+   
+    
+    <!--image slider!-->
+    <div class="cycle-slideshow">
+			<span class="cycle-prev">&#9001;</span>
+			<span class="cycle-next">&#9002;</span>
+			<span class="cycle-pager"></span>
+			<img src="Images/3.png"/>
+			<img src="Images/4.png"/>
+    </div>
+    <!-- end of slider !-->
+</div>
+
+ <div id="about-us">
+        <h2 class="about-us">ABOUT US</h2>
+        <article>
+            <p class="about-para1">The Labour Alliance Uganda Ltd (TLAU) is registered by the Uganda Registration Services Bureau (URSB) and Ministry of Gender Labour Social Development ( GLSD) OSH Department.The Labour Alliance Uganda strives to advocate for, promote and defend employees,employers and other marginalised groups in society against all forms of exploitation, subjugation and enslavement at places of work in Uganda and beyond. TLAU is governed and administered by a Board of Directors and Secretariat located in Kampala City Uganda. we networks and collaborates with social partners like the Labour Union activists, employers’ and employees’ organisations, Government Ministries, think tanks,private sector, Civil Society Organisations (CSOs) and Non-Government Organisations (NGOs) with similar objectives and interests.</p>
+        </article>
+    </div>
+     
+     
+     <div id="second-section">
+        <div class="first-col">
+            <h4 class="comp-title">COMPONENTS OF THE LABOUR ALLIANCE UGANDA</h4>
+            
+            <div id="first-list">
+                <ul>
+                    <li>Work Place Health and Safety</li>
+                    <li>Gender</li>
+                    <li>Monitoring, tracking and Rescue of workers</li>
+                    <li>The labour Relations.</li>
+                </ul>
+            </div>
+            <div id="second-list">
+                 <ul>
+                    <li>Environmental Protection</li>
+                    <li>Work Place wellness through sports</li>
+                    <li>Road Safety</li>
+                    <li>Provision of standard Personel</li>
+                </ul>
+            </div>
+        </div>
+        <div class="second-col">
+            <h4 class="comp-title">OUR CORE VALUES</h4>
+            <ul>
+                <li>Observance of Justice, Human & Workers rights.</li>
+                <li>Transparency, Accountability and Integrity</li>
+                <li>Hard Work, Ethics and Productivity.</li>
+                <li>Networking, lobbying and collaboration with all stakeholder</li>
+                <li>Equity, integrity and service at workplace and in society</li>
+                <li>Dignity, team building and sanctity of the human being.</li>
+            </ul>
+        </div>
+        <div class="third-col">
+            <h4 class="comp-title">OUR STANDARDS</h4>
+            <p>We maintain Occupational Safety and Health standards in a variety of industries and workplaces through demand driven educational and practical sessions through requisite capacity and experience.</p>
+        </div>
+    </div>
+    
+    <div id="third-section">
+        <div id="img1"></div>
+        <div id="img2"></div>
+        <div id="img3"></div>
+    </div>
+    
+    <div id="fourth">
+         <div class="first-cols">
+            <h4 class="comp-title">OUR MAIN OBJECTIVES</h4>
+            <p>To ensure that Ugandan Worker’s Occupational Rights, Interests, Welfare, Awareness and Freedoms are monitored, promoted and protected worldwide.</p>
+            
+            <input type="button" value="Read more"/>
+        </div>
+        
+        <div class="second-cols">
+            <h4 class="comp-title">OUR GOALS</h4>
+            <p>
+                To make sure that all ugandan workers Rights, Interest, Welfare ad freedoms are monitored, promoted and prtected worldwide
+            </p>
+            <input type="button" value="Read more"/>
+            
+        </div>
+        
+        <div class="third-cols">
+            <h4 class="comp-title">OUR STANDARDS</h4>
+            <p>To trigger collective social action within our organisation in conjunction with our partners to become stronger and proactive in monitoring...</p>
+            
+            <p>supporting and defending the marginalised groups against all forms of exploitation,discrimination, enslavement, physical abuse and violation of fundamental Human and Occupational Rights.</p>
+        </div>
+    </div>
+    
+     
+</div>
+
+<div id="footer">
+      <div id="first-footer-box">
+           <h4 class="comp-titles">Contact us</h4>
+                <form>
+                   <div id="form-col1">
+                        <p>name</p>
+                        <input type="text" />
+                        <p>telephone</p>
+                        <input type="text" />
+                    </div>
+
+                     <div id="form-col2">
+                        <p>Email</p>
+                        <input type="email" />
+                        <p>Subject</p>
+                        <input type="text" />
+                    </div>
+                    
+                    <textarea class="content-msg"></textarea>
+                    <input type="submit"/>
+                </form>
+        </div>
+        
+        <div id="second-footer-box">
+            <h4 class="comp-titles">Company Information !</h4>
+            <ul>
+                <li>The Labour Alliance Uganda</li>
+                <li>Registered in Uganda</li>
+            </ul>
+        </div>
+        
+        <div id="last-footer-box">
+            <h4 class="comp-titles">Our Contact Details !</h4>
+            <p>The Labour Alliance Uganda
+             Plot 2, Geoge Street, Simbamayo
+             Building/MLGSD,Level 5 Office 1
+             Kampala
+            </p>
+            
+            <div id="phone">
+                <p>Tel: +2560392001761</p>
+                <p>P.O Box 23629 Kla</p>
+                <p>Email: info@thelabouralliances.org</p>
+                
+            </div>
+        </div>
+    </div>
+
+<script src="jquery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.cycle2.min.js"></script>
+ 
+<script type="text/javascript">
+    
+    $(function(){
+        
+    $('#play').click(function(){$('#slider').cycle('resume'); return false;});
+    
+     $('#pause').click(function(){$('#slider').cycle('pause'); return false;});
+        
+    $('#slider').cycle({
+        fx: 'scrollHorz',
+        next: '#next',
+        prev: '#prev',
+        pager: '#pager',
+        timeout: 3000,
+        speed: 900
+    });
+    });
+</script>
+</body>
+</html>
